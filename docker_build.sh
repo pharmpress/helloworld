96cf8e5062819258516e7a200bf38b1f4d42f3b9
Ramiro.Calle <ramiro.calle@rpharms.com>
2015-07-09 13:47:19 +0100

    fix current directory resolution

--- docker_build.sh
+++ docker_build.sh
@@ -6,8 +6,8 @@ go run third_party.go install -tags netgo std
 CGO_ENABLED=0 go run third_party.go build -a -tags netgo --ldflags '-w -extldflags=-static' -o bin/helloworld-linux64-static github.com/pharmpress/helloworld
 EOF
 
-echo "building statically-linked helloworld..."
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:latest bash .payload
+echo "building statically-linked helloworld in ..."
+docker run --rm -v $(pwd):/usr/src/myapp -w /usr/src/myapp golang:latest bash .payload
 rm -f .payload
 
 
